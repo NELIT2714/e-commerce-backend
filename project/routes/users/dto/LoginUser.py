59d0de7026f597b65828c8cc1ca9c2e42d1df89b
NELIT2714 <nelit2714@gmail.com>
2025-01-27 00:29:20 +0100

    chore: change dto structure in LoginUser pydantic class, add example request body

--- project/routes/users/dto/LoginUser.py
+++ project/routes/users/dto/LoginUser.py
@@ -1,6 +1,26 @@
 from pydantic import BaseModel
 
+from project.routes.users.dto.NewUser import MetaData
+
 
 class LoginUser(BaseModel):
     username: str
     password: str
+    metadata: MetaData
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "username": "ExampleUsername",
+                    "password": "Qwerty123_@",
+                    "metadata": {
+                        "os": "IOS",
+                        "browser": "Safari",
+                        "device": "IPhone 15",
+                        "ip": "81.65.35.187"
+                    }
+                }
+            ]
+        }
+    }
