a6c07cda7aeaadd80173d3a4efda4596b4cfe75a
NELIT2714 <nelit2714@gmail.com>
2025-02-02 01:21:56 +0100

    feat: add middlewares

---
+++ project/middlewares/check_api_key.py
@@ -0,0 +1,17 @@
+from fastapi import Request
+from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class APIKeyMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app, api_key: str):
+        super().__init__(app)
+        self.api_key = api_key
+
+    async def dispatch(self, request: Request, call_next):
+        api_key = request.headers.get("X-API-Key")
+        if not api_key == self.api_key:
+            return JSONResponse(status_code=403, content={"error": "Invalid API key"})
+        return await call_next(request)
+    
